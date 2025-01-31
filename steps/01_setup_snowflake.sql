6117fd8d0973c56b5d14b4ce6b0386b770e7f88d
parashurampatil <72278136+parashurampatil@users.noreply.github.com>
2025-01-31 21:19:31 +0000

    Changed the demo files

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -7,7 +7,7 @@ CREATE OR ALTER WAREHOUSE QUICKSTART_WH__{{environment}}
 
 
 -- Separate database for git repository
-CREATE OR ALTER DATABASE QUICKSTART__{{environment}};
+CREATE OR ALTER DATABASE QUICKSTART_COMMON;
 
 
 -- API integration is needed for GitHub integration
@@ -22,7 +22,7 @@ CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
   ORIGIN = 'https://github.com/parashurampatil/snowflake-devops-demo.git'; -- INSERT URL OF FORKED REPO HERE
 
-
+CREATE OR ALTER DATABASE QUICKSTART_{{environment}};
 
 
 -- To monitor data pipeline's completion
